f7383325fe774e1c50b26625ca050c4fe2b38794
Ian Strasma <ianstrasma@gmail.com>
2021-08-30 14:05:12 -0500

    Add a fix for the search function to properly return true when a value is found within the linked list

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -38,9 +38,14 @@ bool LinkedList<T>::search(T value) const
 	Node<T>* temp = m_front;
 	bool isFound = false;
 
-	/** TODO 
-		Fix this method
-	*/
+	while( temp != nullptr )
+	{
+		if (temp->getValue() == value)
+		{
+			isFound = true;
+		}
+		temp = temp->getNext();
+	}
 
 	return(isFound);
 }
@@ -117,9 +122,6 @@ bool LinkedList<T>::removeBack()
 		m_size --;
 		isRemoved = true;	
 	}
-	/** TODO 
-		Fix this method
-	*/
 
 	return(isRemoved);
 }	
